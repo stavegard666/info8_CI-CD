8f6e79255c99c6938299962f0fca3b22d14167a6
Antoine Malmezac <antoinemalmezac@MacBook-Air-de-Antoine.local>
2025-03-23 03:24:23 +0100

    Fix: Contracts

--- shared-contracts/src/main/java/com/epita/contracts/LikesContract.java
+++ shared-contracts/src/main/java/com/epita/contracts/LikesContract.java
@@ -3,46 +3,16 @@ package com.epita.contracts;
 import java.time.Instant;
 import java.util.UUID;
 
-import org.bson.codecs.pojo.annotations.BsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+
+@AllArgsConstructor
+@Data
 public class LikesContract {
 
-    @BsonProperty("_id")
-    private UUID id;
     private UUID userId;
     private UUID postId;
     private Instant likedAt;
 
-    public LikesContract() {
-    }
-
-    public LikesContract(UUID userId, UUID postId, Instant likedAt) {
-        this.userId = userId;
-        this.postId = postId;
-        this.likedAt = likedAt;
-    }
-
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
-
-    public UUID getPostId() {
-        return postId;
-    }
-
-    public void setPostId(UUID postId) {
-        this.postId = postId;
-    }
-
-    public Instant getLikedAt() {
-        return likedAt;
-    }
-
-    public void setLikedAt(Instant likedAt) {
-        this.likedAt = likedAt;
-    }
 }
