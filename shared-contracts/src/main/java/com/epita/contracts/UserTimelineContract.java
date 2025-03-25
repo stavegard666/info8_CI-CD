b9e5018f45758746a01150a4baa2e3d7bfdd5a05
Antoine Malmezac <antoinemalmezac@MacBook-Air-de-Antoine.local>
2025-03-25 17:48:34 +0100

    Fix: Contracts & Dependecies

--- shared-contracts/src/main/java/com/epita/contracts/UserTimelineContract.java
+++ shared-contracts/src/main/java/com/epita/contracts/UserTimelineContract.java
@@ -3,14 +3,17 @@ package com.epita.contracts;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class UserTimelineContract {
     private UUID userId;
-    private List<Object> timeline;
+    private List<PostConstruct> timeline;
 
 }
